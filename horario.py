d0c911f5784c666224e115c054e8d8bcfa904ffd
Fernandes6767 <2010.diogofernandes@gmail.com>
2026-05-06 08:27:19 +0100

    Update horario.py

--- horario.py
+++ horario.py
@@ -1,27 +1,15 @@
 # horario.py
 
 from turmas import turmas
-
+from curso import validar_id, validar_nome, validar_duracao
+from utils import validar_id , validar_lista
 horarios = {}
-
-
-# ==============================
-# VALIDACOES
-# ==============================
-def _validar_id(id_horario):
-    return isinstance(id_horario, str) and len(id_horario.strip()) > 0
-
-
-def _validar_lista(lista):
-    return isinstance(lista, list)
-
-
 # ==============================
 # CREATE
 # ==============================
 def criar_horario(id_horario, id_turma, lista_disciplina, lista_professor):
 
-    if not _validar_id(id_horario):
+    if not validar_id(id_horario):
         return 400, "ID invalido"
 
     if id_horario in horarios:
@@ -30,10 +18,10 @@ def criar_horario(id_horario, id_turma, lista_disciplina, lista_professor):
     if id_turma not in turmas:
         return 404, "Turma nao existe"
 
-    if not _validar_lista(lista_disciplina):
+    if not validar_lista(lista_disciplina):
         return 400, "Lista de disciplinas invalida"
 
-    if not _validar_lista(lista_professor):
+    if not validar_lista(lista_professor):
         return 400, "Lista de professores invalida"
 
     # ==============================
@@ -78,12 +66,12 @@ def atualizar_horario(id_horario, novas_disciplinas=None, novos_professores=None
         return 404, "Horario nao encontrado"
 
     if novas_disciplinas is not None:
-        if not _validar_lista(novas_disciplinas):
+        if not validar_lista(novas_disciplinas):
             return 400, "Lista invalida"
         horarios[id_horario]["lista_disciplina"] = novas_disciplinas
 
     if novos_professores is not None:
-        if not _validar_lista(novos_professores):
+        if not validar_lista(novos_professores):
             return 400, "Lista invalida"
         horarios[id_horario]["lista_professor"] = novos_professores
 
